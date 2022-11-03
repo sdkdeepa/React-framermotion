724c2b33f12aa1052543730781ac1d22791aa6e3
sdkdeepa <deepa94086@gmail.com>
2022-11-02 17:35:42 -0700

    Update Header.js

--- piesjoint/src/components/Header.js
+++ piesjoint/src/components/Header.js
@@ -4,25 +4,10 @@ import { motion } from 'framer-motion';
 const Header = () => {
   return (
     <header>
-      {/* <div className="logo">
-        <svg className="pie-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
-            fill="none"
-            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-          />
-          <path
-            fill="none"
-            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-          />
-        </svg>
-      </div> */}
      <motion.div className="title"
           initial={{ scale: 0 }}
           animate={{ rotate: 360, scale: 1}}
           transition={{type: "spring",stiffness: 120,damping: 20}}
-        // initial={{ y: -250}}
-        // animate={{ y: -10 }}
-        // transition={{ delay: 0.2, type: 'spring', stiffness: 200 }}
       >
         <h1>Pies 'o mania</h1>
       </motion.div>
